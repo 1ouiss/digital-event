66d00e956a62c05641d9ac16a9662d73d53de86b
1ouiss <ls.bouet@gmail.com>
2024-02-04 23:14:34 +0100

    feat: create context

---
+++ app/src/context/Context.tsx
@@ -0,0 +1,71 @@
+import { FC, createContext, useEffect, useState } from "react";
+import { ContextType, GameType, PlayerType, Props } from "./utils";
+import { collection, onSnapshot } from "firebase/firestore";
+import { db } from "../utils/firebase";
+
+const Context = createContext({} as ContextType);
+
+const ContextProvider: FC<Props> = ({ children }) => {
+  const [game, setGame] = useState({} as GameType);
+  const [player1, setPlayer1] = useState({} as PlayerType);
+  const [player2, setPlayer2] = useState({} as PlayerType);
+
+  const getPlayer1 = () => {
+    const collectionRef = collection(db, "player1");
+    onSnapshot(collectionRef, async (querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        setPlayer1({
+          id: doc.id,
+          ...doc.data(),
+        } as PlayerType);
+      });
+    });
+  };
+
+  const getPlayer2 = () => {
+    const collectionRef = collection(db, "player2");
+    onSnapshot(collectionRef, async (querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        setPlayer2({
+          id: doc.id,
+          ...doc.data(),
+        } as PlayerType);
+      });
+    });
+  };
+
+  const getGame = () => {
+    const collectionRef = collection(db, "game");
+    onSnapshot(collectionRef, async (querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        setGame({
+          id: doc.id,
+          ...doc.data(),
+        } as GameType);
+      });
+    });
+  };
+
+  useEffect(() => {
+    getPlayer1();
+    getPlayer2();
+    getGame();
+  }, []);
+
+  return (
+    <Context.Provider
+      value={{
+        game,
+        setGame,
+        player1,
+        setPlayer1,
+        player2,
+        setPlayer2,
+      }}
+    >
+      {children}
+    </Context.Provider>
+  );
+};
+
+export { Context, ContextProvider };
---
+++ app/src/context/utils.ts
@@ -0,0 +1,25 @@
+export type GameType = {
+  id: string;
+  isActive: boolean;
+  player1Domination: number;
+  gameStarted: boolean;
+  chronoStarted: boolean;
+};
+
+export type PlayerType = {
+  id: string;
+  combination: number[];
+};
+
+export type ContextType = {
+  game: GameType;
+  setGame: (game: GameType) => void;
+  player1: PlayerType;
+  setPlayer1: (player: PlayerType) => void;
+  player2: PlayerType;
+  setPlayer2: (player: PlayerType) => void;
+};
+
+export type Props = {
+  children: JSX.Element;
+};
